75b6932fe6668a6894579f86c3d653d68c3c2cb5
MaicoBalawag <dummyacchenz@gmail.com>
2025-10-16 19:39:22 +0800

    Add files via upload
    Uploaded C case problem and output screenshot

---
+++ Activity.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1 = 15, num2 = 25, sum;
+    sum = num1 + num2;
+
+    printf("Name: Maico Bualan Balawag\n");
+    printf("Id Number: 2025303964\n");
+    printf("--------------------------------\n");
+    printf("Result: The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+    
+}
